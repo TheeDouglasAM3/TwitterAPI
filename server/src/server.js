3276d1ca89772a316bfcbc703e06809c6de6fee5
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-24 22:34:26 -0300

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,3 @@
-const express = require('express')  
-const cors = require('cors')
-const routes = require('./routes')
+const app = require('./app');
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-
-app.listen(process.env.PORT || 3333)
+app.listen(process.env.PORT || 3333);
