944b95fa411fb560570cce3eeeadddecd70409c5
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-24 22:35:25 -0300

    Fix getFilteredStreamRules.js

--- server/src/getFilteredStreamRules.js
+++ server/src/getFilteredStreamRules.js
@@ -1,41 +1,36 @@
 require('dotenv/config');
 const needle = require('needle');
-
-// The code below sets the bearer token from your environment variables
-// To set environment variables on Mac OS X, run the export command below from the terminal: 
-// export BEARER_TOKEN='YOUR-TOKEN' 
 const token = process.env.BEARER_TOKEN; 
-
 const endpointUrl = 'https://api.twitter.com/2/tweets/search/stream/rules'
 
-async function getRequest() {
+module.exports = {
 
-    // Edit query parameters below
-    const params = {
+    async getFilteredStreamRules()  {
+        try {
+            // Make request
+            const response = await this.getRequest();
+            return response
+            
+        } catch(e) {
+            console.log(e);
+            process.exit(-1);
+        }
+    },
 
-    } 
+    async getRequest() {
 
-    const res = await needle('get', endpointUrl, params, { headers: {
-        "authorization": `Bearer ${token}`
-    }})
+        const params = {
 
-    if(res.body) {
-        return res.body
-    } else {
-        throw new Error ('Unsuccessful request')
-    }
+        } 
+
+        const res = await needle('get', endpointUrl, params, { headers: {
+            "authorization": `Bearer ${token}`
+        }})
+
+        if(res.body) {
+            return res.body
+        } else {
+            throw new Error ('Unsuccessful request')
+        }       
+    },
 }
-
-(async () => {
-
-    try {
-        // Make request
-        const response = await getRequest();
-        console.log(response)
-
-    } catch(e) {
-        console.log(e);
-        process.exit(-1);
-    }
-    process.exit();
-  })();
