0c8509d6d39aa624c6a85935e0961a2f38cbd65d
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-26 20:44:24 -0300

    Update Routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,16 +1,33 @@
-const express = require('express');
-const routes = express.Router();
+const express = require('express')
+const routes = express.Router()
 
-const stream = require('./getFilteredStreamRules');
+const filteredStreamController = require('./Controller/FilteredStreamController')
 
 routes.get('/stream/rules', async (req, res, next) => {
   try {
-    const data = await stream.getFilteredStreamRules()
+    const data = await filteredStreamController.index()
     res.json(data)
   } catch (e) {
-    //this will eventually be handled by your error handling middleware
     next(e) 
   }
-});
+})
+
+routes.post('/stream/rules', async (req, res, next) => {
+  try {
+    const data = await filteredStreamController.store()
+    res.json(data)
+  } catch (e) {
+    next(e) 
+  }
+})
+
+routes.delete('/stream/rules', async (req, res, next) => {
+  try {
+    const data = await filteredStreamController.delete()
+    res.json(data)
+  } catch (e) {
+    next(e) 
+  }
+})
 
 module.exports = routes;
