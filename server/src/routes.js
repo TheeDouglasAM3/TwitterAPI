d405fe8cde2490ff71a0d7805cf0bdc9145bcb40
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-26 21:31:24 -0300

    Update Routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = express.Router()
 
 const filteredStreamController = require('./Controller/FilteredStreamController')
+const streamTweetsController = require('./Controller/StreamTweetsController')
 
 routes.get('/stream/rules', async (req, res, next) => {
   try {
@@ -24,6 +25,15 @@ routes.post('/stream/rules', async (req, res, next) => {
 routes.delete('/stream/rules', async (req, res, next) => {
   try {
     const data = await filteredStreamController.delete()
+    res.json(data)
+  } catch (e) {
+    next(e) 
+  }
+})
+
+routes.get('/stream', async (req, res, next) => {
+  try {
+    const data = await streamTweetsController.index()
     res.json(data)
   } catch (e) {
     next(e) 
